050bca898b9b6c8da53a30cd6c8b10fcee4c8eca
JorgeFranklin <jorgesowetoo@gmail.com>
2023-05-03 15:45:40 -0300

    recipes-app

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -30,7 +30,7 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
 
-  secret: process.env.NEXT_AUTH_SECRET,
+  secret: process.env.SECRET,
 
   callbacks: {
     async signIn({ user }) {
